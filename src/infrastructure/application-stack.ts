7e20c31ce1750ee248e7bd354a64ab000045b37e
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-03 21:42:37 +0100

    added deployment stuff

--- src/infrastructure/application-stack.ts
+++ src/infrastructure/application-stack.ts
@@ -1,8 +1,26 @@
 import * as cdk from "aws-cdk-lib"
+import * as s3 from "aws-cdk-lib/aws-s3"
+import * as s3Deploy from "aws-cdk-lib/aws-s3-deployment"
+
 import { Construct } from "constructs"
 
+interface ApplicationStackProps extends cdk.StackProps {
+  environmentName: string
+}
+
 export class ApplicationStack extends cdk.Stack {
-  constructor(scope: Construct, props: cdk.StackProps) {
+  constructor(scope: Construct, props: ApplicationStackProps) {
     super(scope, "TnmV2ApplicationStack", props)
+
+    const deployBucket = new s3.Bucket(this, "TnmV2DeployBucket", {
+      bucketName: `${props.environmentName}-tnm-v2-deploy-bucket`,
+      publicReadAccess: true,
+      websiteIndexDocument: "index.html",
+    })
+
+    new s3Deploy.BucketDeployment(this, "TnmV2Deploy", {
+      sources: [s3Deploy.Source.asset("../../public")],
+      destinationBucket: deployBucket,
+    })
   }
 }
