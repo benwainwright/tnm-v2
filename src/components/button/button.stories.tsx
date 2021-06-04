30de98241f51b706599effc91ddc564e4ad09658
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-04 16:29:09 +0100

    Setup storybook

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -3,14 +3,14 @@ import { Story, Meta } from "@storybook/react"
 import Button, { ButtonProps } from "./button"
 
 export default {
-  title: "Example/Button",
+  title: "TNM/Button",
   component: Button,
   argTypes: {
     backgroundColor: { control: "color" },
   },
 } as Meta
 
-const Template: Story<ButtonProps> = args => <Button {...args} />
+const Template: Story<ButtonProps> = args => <Button {...args}>Click Me</Button>
 
 export const Primary = Template.bind({})
 Primary.args = {}
