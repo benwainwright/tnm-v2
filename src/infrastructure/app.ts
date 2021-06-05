642078cace263038fc5c6eb427937180a363e07f
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-05 17:51:51 +0100

    Changed entry file

--- src/infrastructure/app.ts
+++ src/infrastructure/app.ts
@@ -1,15 +1,23 @@
 import * as cdk from "aws-cdk-lib"
 import { ApplicationStack } from "./application-stack"
 
+import { PipelineStack } from "./pipeline-stack"
+
 const app = new cdk.App()
 
 const environmentName =
   process.env.DEPLOY_ENVIRONMENT === "prod" ? "prod" : "test"
 
+const env = {
+  account: "426446031572",
+  region: "eu-west-2",
+}
+
 new ApplicationStack(app, {
   environmentName,
-  env: {
-    account: "426446031572",
-    region: "eu-west-2",
-  },
+  env,
+})
+
+new PipelineStack(app, {
+  env,
 })
