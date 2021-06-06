660ff0b429cd6c5a2b817a95872318e18c13aad4
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-06 16:24:32 +0100

    Added tests for useAxe

---
+++ src/hooks/useAxe.spec.ts
@@ -0,0 +1,31 @@
+import { renderHook, act, waitFor } from "@testing-library/react-hooks";
+import { useAxe } from "./useAxe";
+import { mocked } from "ts-jest/utils";
+import axe from "@axe-core/react";
+import React from "react";
+import ReactDom from "react-dom";
+
+jest.mock("react-dom");
+jest.mock("@axe-core/react");
+
+describe("use axe", () => {
+  afterEach(() => {
+    delete process.env.NODE_ENV;
+    jest.resetAllMocks();
+  });
+
+  it("should pass react and reactdom into the axe function", async () => {
+    const { waitFor } = renderHook(() => useAxe());
+
+    await waitFor(() =>
+      expect(mocked(axe, true)).toBeCalledWith(React, ReactDom, 1000)
+    );
+  });
+
+  it("should not do anything in production", async () => {
+    process.env.NODE_ENV = "production";
+    renderHook(() => useAxe());
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    expect(mocked(axe, true)).not.toBeCalled();
+  });
+});
