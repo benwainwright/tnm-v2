15d08cf527e952ed9c35e4a3980659d6a728fd50
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-08 22:37:50 +0100

    Change string

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `TNM Admin V2`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `Manage customer orders`,
     author: `@gatsbyjs`,
   },
   plugins: [
