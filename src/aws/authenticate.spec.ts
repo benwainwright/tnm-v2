c028273f6449a6088af92e1775d2a76f0375fec3
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-11 11:04:20 +0100

    Fix authenticate tests

--- src/aws/authenticate.spec.ts
+++ src/aws/authenticate.spec.ts
@@ -1,13 +1,41 @@
 import { Auth } from "aws-amplify";
 import { when } from "jest-when";
 import { mocked } from "ts-jest/utils";
+import { getPoolConfig } from "./getPoolConfig";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 import * as authenticate from "./authenticate";
 
 jest.mock("aws-amplify");
+jest.mock("aws-sdk");
+jest.mock("./getPoolConfig");
+
+const mockParamsFunction = jest.fn();
+
+jest.mock("aws-sdk", () => {
+  return {
+    CognitoIdentityServiceProvider: class {
+      respondToAuthChallenge(mockParams) {
+        mockParamsFunction(mockParams);
+        return this;
+      }
+
+      promise() {
+        return Promise.resolve();
+      }
+    },
+  };
+});
 
 describe("The authenticate module", () => {
   describe("login()", () => {
     it("returns the promise from Auth.signIn", async () => {
+      mocked(getPoolConfig).mockReturnValue({
+        UserPoolId: "pool-id",
+        ClientId: "client-id",
+        RedirectUrl: "redirect-url",
+        AuthUrl: "auth-url",
+      });
+
       when(mocked(Auth.signIn))
         .calledWith("foo", "bar")
         .mockResolvedValue("loginResponse");
@@ -20,6 +48,13 @@ describe("The authenticate module", () => {
 
   describe("signOut()", () => {
     it("returns the promise from Auth.logout", async () => {
+      mocked(getPoolConfig).mockReturnValue({
+        UserPoolId: "pool-id",
+        ClientId: "client-id",
+        RedirectUrl: "redirect-url",
+        AuthUrl: "auth-url",
+      });
+
       mocked(Auth.signOut).mockResolvedValue("logoutResponse");
 
       const result = await authenticate.signOut();
@@ -28,8 +63,48 @@ describe("The authenticate module", () => {
     });
   });
 
+  describe("newPasswordChallengeResponse", () => {
+    it("calls the respondToAuthChallenge method with the correct params", async () => {
+      mocked(getPoolConfig).mockReturnValue({
+        UserPoolId: "pool-id",
+        ClientId: "client-id",
+        RedirectUrl: "redirect-url",
+        AuthUrl: "auth-url",
+      });
+
+      const sessionValue = "the-session";
+      const usernameValue = "the-username";
+      const passwordValue = "the-password";
+
+      await authenticate.newPasswordChallengeResponse(
+        usernameValue,
+        passwordValue,
+        sessionValue
+      );
+
+      expect(mockParamsFunction).toHaveBeenCalledWith(
+        expect.objectContaining({
+          ClientId: "client-id",
+          ChallengeName: "NEW_PASSWORD_REQUIRED",
+          Session: sessionValue,
+          ChallengeResponses: {
+            USERNAME: usernameValue,
+            NEW_PASSWORD: passwordValue,
+          },
+        })
+      );
+    });
+  });
+
   describe("currentUser()", () => {
     it("returns the promise from Auth.currentAuthenticatedUser", async () => {
+      mocked(getPoolConfig).mockReturnValue({
+        UserPoolId: "pool-id",
+        ClientId: "client-id",
+        RedirectUrl: "redirect-url",
+        AuthUrl: "auth-url",
+      });
+
       mocked(Auth.currentAuthenticatedUser).mockResolvedValue(
         "currentUserResponse"
       );
@@ -40,6 +115,13 @@ describe("The authenticate module", () => {
     });
 
     it("returns undefined if currentAuthenticatedUser throws", async () => {
+      mocked(getPoolConfig).mockReturnValue({
+        UserPoolId: "pool-id",
+        ClientId: "client-id",
+        RedirectUrl: "redirect-url",
+        AuthUrl: "auth-url",
+      });
+
       mocked(Auth.currentAuthenticatedUser).mockRejectedValue(
         new Error("Whoops!")
       );
--- src/aws/authenticate.ts
+++ src/aws/authenticate.ts
@@ -1,29 +1,65 @@
 import { Auth } from "aws-amplify";
 import { getPoolConfig } from "./getPoolConfig";
 import backendOutputs from "../backend-outputs.json";
+import { CognitoIdentityServiceProvider } from "aws-sdk";
 
-const outputs = getPoolConfig(backendOutputs);
+const getConfigurer = () => {
+  let configureDone = false;
+  return () => {
+    const outputs = getPoolConfig(backendOutputs);
+    if (!configureDone) {
+      Auth.configure({
+        Auth: {
+          region: "eu-west-2",
+          userPoolId: outputs.UserPoolId,
+          userPoolWebClientId: outputs.ClientId,
+        },
+      });
+      configureDone = true;
+    }
+    return outputs;
+  };
+};
 
-Auth.configure({
-  Auth: {
-    region: "eu-west-2",
-    userPoolId: outputs.UserPoolId,
-    userPoolWebClientId: outputs.ClientId,
-  },
-});
+const configureAuth = getConfigurer();
 
 export const login = async (username: string, password: string) => {
+  configureAuth();
   return Auth.signIn(username, password);
 };
 
 export const signOut = async () => {
+  configureAuth();
   return Auth.signOut();
 };
 
 export const currentUser = async () => {
+  configureAuth();
   try {
     return await Auth.currentAuthenticatedUser();
   } catch (error) {
     return undefined;
+  }
+};
+
+export const newPasswordChallengeResponse = async (
+  username: string,
+  password: string,
+  session: string
+) => {
+  const cognito = new CognitoIdentityServiceProvider();
+  const config = configureAuth();
+  if (config) {
+    return cognito
+      .respondToAuthChallenge({
+        ClientId: config.ClientId,
+        ChallengeName: "NEW_PASSWORD_REQUIRED",
+        Session: session,
+        ChallengeResponses: {
+          USERNAME: username,
+          NEW_PASSWORD: password,
+        },
+      })
+      .promise();
   }
 };
