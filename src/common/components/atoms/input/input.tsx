e04e2b55a2303710a54f05d730e9837950fe27a6
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-11 16:04:08 +0100

    Add error message to fields

--- src/common/components/atoms/input/input.tsx
+++ src/common/components/atoms/input/input.tsx
@@ -1,21 +1,35 @@
-import { FC, ChangeEvent } from "react"
+import { FC, ChangeEvent } from "react";
 
-import styled from "@emotion/styled"
+import styled from "@emotion/styled";
 
-import { LABEL_TEXT, BUTTON_BLACK } from "../../../config"
+import { LABEL_TEXT, BUTTON_BLACK } from "../../../config";
 
 const InputContainer = styled.div`
   font-family: "Acumin Pro";
   display: flex;
   flex-direction: column;
-`
+`;
+
+const LabelRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 0.2rem;
+`;
+
+const ErrorLabel = styled.label`
+  font-family: "Acumin Pro";
+  color: red;
+  padding-bottom: 0.5rem;
+`;
+
+ErrorLabel.displayName = "label";
 
 const InputLabel = styled.label`
   font-family: "Acumin Pro";
   color: ${LABEL_TEXT};
   padding-bottom: 0.5rem;
-`
-InputLabel.displayName = "label"
+`;
+InputLabel.displayName = "label";
 
 const InputField = styled.input`
   font-family: "Acumin Pro";
@@ -24,23 +38,28 @@ const InputField = styled.input`
   border: 1px solid ${BUTTON_BLACK};
   line-height: 1.5rem;
   padding: 0.5rem 0.5rem;
-`
-InputField.displayName = "input"
+`;
+InputField.displayName = "input";
 
 export interface InputProps {
-  label?: string
-  placeholder?: string
-  value?: string
-  name?: string
-  type?: string
-  errorMessage?: string
-  onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+  label?: string;
+  placeholder?: string;
+  value?: string;
+  name?: string;
+  type?: string;
+  errorMessage?: string;
+  onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input: FC<InputProps> = props => {
+const Input: FC<InputProps> = (props) => {
   return (
     <InputContainer>
-      <InputLabel htmlFor={props.name}>{props.label}</InputLabel>
+      <LabelRow>
+        <InputLabel htmlFor={props.name}>{props.label}</InputLabel>
+        {props.errorMessage ? (
+          <ErrorLabel htmlFor={props.name}>{props.errorMessage}</ErrorLabel>
+        ) : null}
+      </LabelRow>
       <InputField
         id={props.name}
         name={props.name}
@@ -50,7 +69,7 @@ const Input: FC<InputProps> = props => {
         placeholder={props.placeholder}
       />
     </InputContainer>
-  )
-}
+  );
+};
 
-export default Input
+export default Input;
