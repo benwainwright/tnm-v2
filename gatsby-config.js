df210fe4e51a7b5505b3163b5c8d7acd4939218b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-14 17:19:14 +0100

    Fix build failures

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
+        path: `${__dirname}/src/common/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
