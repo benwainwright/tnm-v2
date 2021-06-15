4879de7a50661b570898680a05519021636177da
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-15 11:00:59 +0100

    Add another test

--- src/common/components/molecules/header/desktop-header.spec.tsx
+++ src/common/components/molecules/header/desktop-header.spec.tsx
@@ -1,3 +1,14 @@
+import DesktopHeader from "./desktop-header"
+import { shallow } from "enzyme"
+
 describe("The desktop header", () => {
-  it("contains an Our Story link", () => {})
+  it("contains an Our Story link", () => {
+    const wrapper = shallow(<DesktopHeader />)
+
+    const ourStoryLink = wrapper
+      .find("a")
+      .findWhere((link) => link.prop("href") === "/our-story/")
+
+    expect(ourStoryLink).toHaveLength(1)
+  })
 })
