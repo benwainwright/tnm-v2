7c9196046b6b43ca0eb74ee494f94482bb8c6054
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-17 00:05:07 +0100

    Add test for seed cognito

--- src/scripts/lib/seed-cognito.ts
+++ src/scripts/lib/seed-cognito.ts
@@ -1,3 +1,21 @@
-const TEST_POOL_ID = "eu-west-2_STGr0ooNo"
+import { CognitoIdentityServiceProvider } from "aws-sdk"
 
-export const seedCognito = async () => {}
+export const TEST_USER = "cypress-test-user"
+
+export const seedCognito = async () => {
+  const cognito = new CognitoIdentityServiceProvider()
+
+  const poolId = process.env.CYPRESS_POOL_ID
+
+  if (!poolId) {
+    // eslint-disable-next-line fp/no-throw
+    throw new Error("CYPRESS_POOL_ID not configured")
+  }
+
+  await cognito
+    .adminDeleteUser({
+      UserPoolId: poolId,
+      Username: TEST_USER,
+    })
+    .promise()
+}
