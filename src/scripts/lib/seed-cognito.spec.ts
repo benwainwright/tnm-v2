1ce085448072c554b6aa2cca1a74fa1059e3b82a
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-17 00:20:25 +0100

    Basic test-user seed

--- src/scripts/lib/seed-cognito.spec.ts
+++ src/scripts/lib/seed-cognito.spec.ts
@@ -1,7 +1,10 @@
 import AWSMock from "aws-sdk-mock"
 import AWS from "aws-sdk"
 import { seedCognito, TEST_USER } from "./seed-cognito"
-import { AdminDeleteUserRequest } from "aws-sdk/clients/cognitoidentityserviceprovider"
+import {
+  AdminDeleteUserRequest,
+  AdminCreateUserRequest,
+} from "aws-sdk/clients/cognitoidentityserviceprovider"
 
 describe("seed cognito", () => {
   beforeEach(() => {
@@ -26,6 +29,18 @@ describe("seed cognito", () => {
       mockAdminDeleteUser
     )
 
+    const mockAdminCreateUser = jest.fn(
+      (params: AdminCreateUserRequest, callback: Function) => {
+        callback()
+      }
+    )
+
+    AWSMock.mock(
+      "CognitoIdentityServiceProvider",
+      "adminCreateUser",
+      mockAdminCreateUser
+    )
+
     await seedCognito()
 
     expect(mockAdminDeleteUser).toBeCalledWith(
@@ -37,5 +52,64 @@ describe("seed cognito", () => {
     )
 
     delete process.env.CYPRESS_POOL_ID
+  })
+
+  it("recreates the test-user", async () => {
+    process.env.CYPRESS_POOL_ID = "foo-id"
+
+    const mockAdminDeleteUser = jest.fn(
+      (params: AdminDeleteUserRequest, callback: Function) => {
+        callback()
+      }
+    )
+
+    AWSMock.mock(
+      "CognitoIdentityServiceProvider",
+      "adminDeleteUser",
+      mockAdminDeleteUser
+    )
+
+    const mockAdminCreateUser = jest.fn(
+      (params: AdminCreateUserRequest, callback: Function) => {
+        callback()
+      }
+    )
+
+    AWSMock.mock(
+      "CognitoIdentityServiceProvider",
+      "adminCreateUser",
+      mockAdminCreateUser
+    )
+
+    await seedCognito()
+
+    expect(mockAdminCreateUser).toBeCalledWith(
+      {
+        UserPoolId: "foo-id",
+        Username: TEST_USER,
+        TemporaryPassword: "520972vi123A.",
+        MessageAction: "Suppress",
+        DesiredDeliveryMediums: ["EMAIL"],
+        UserAttributes: [
+          {
+            Name: "email_verified",
+            Value: "True",
+          },
+          {
+            Name: "phone_number_verified",
+            Value: "True",
+          },
+          {
+            Name: "email",
+            Value: "testing@user.com",
+          },
+          {
+            Name: "phone_number",
+            Value: "+447732432435",
+          },
+        ],
+      },
+      expect.anything()
+    )
   })
 })
