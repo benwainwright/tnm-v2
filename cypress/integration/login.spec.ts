98172b5e6ceb33a99ed2d65d3abac4de96394c28
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-17 20:42:07 +0100

    Seed database

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,7 +1,17 @@
 describe("The login page", () => {
+  beforeEach(() => {
+    cy.exec("yarn ts-node src/scripts/seed-cognito.ts")
+  })
+
   it("Should load a page with a login form", () => {
     cy.visit("/login/")
+    cy.get("form").get("h2").contains("Login")
+  })
 
-    cy.get("form")
+  it("Should display an error message if your password is incorrect", () => {
+    cy.get("form").get("input[name='email']").type("testing@user.com")
+    cy.get("form").get("input[name='password']").type("asdsdfasd")
+    cy.get("form").get("button").contains("Login").click()
+    cy.get("form").contains("Incorrect username or password")
   })
 })
