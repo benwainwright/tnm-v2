78fd09800842a200181a7cae1eb5862c6a3cff57
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-17 22:43:22 +0100

    Added icon to hero

--- src/common/pages/login.tsx
+++ src/common/pages/login.tsx
@@ -2,6 +2,7 @@ import { FC, useState } from "react"
 import Layout from "../components/layout"
 import { ErrorResponse } from "../types/error-response"
 import { LoginBox, MfaBox, NewPasswordBox } from "../components/molecules"
+import AccountIcon from "@common/assets/images/icons/TNM_Icons_Final_Account.png"
 import Seo from "@common/components/seo"
 import { Hero } from "@common/components/containers"
 import { handleLogin } from "../handlers/handle-login"
@@ -24,10 +25,20 @@ const getLoginBox = (state: LoginState) => {
   }
 }
 
-const YourAccountHeader = styled("h1")`
-  font-size: 40px;
+const YourAccountHeaderBox = styled("div")`
   text-align: center;
   color: #3b7d7a;
+  align-items: center;
+  flex-direction: row;
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+`
+
+const YourAccountHeader = styled("h1")`
+  font-size: 40px;
+  display: auto;
+  margin: 0.5rem 0 0 0;
 `
 
 const Login: FC = () => {
@@ -41,7 +52,10 @@ const Login: FC = () => {
     <Layout>
       <Seo title="Login" />
       <Hero>
-        <YourAccountHeader>Your Account</YourAccountHeader>
+        <YourAccountHeaderBox>
+          <img src={AccountIcon} alt="" height="80" width="80" />
+          <YourAccountHeader>Your Account</YourAccountHeader>
+        </YourAccountHeaderBox>
       </Hero>
       <Box
         errors={errorMessage ? [errorMessage] : undefined}
