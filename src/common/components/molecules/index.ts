d16629cd749d49500325c5e2832112bf3ef9a89b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 10:58:53 +0100

    Added quantity stepper to molecules bucket

--- src/common/components/molecules/index.ts
+++ src/common/components/molecules/index.ts
@@ -3,5 +3,6 @@ import { Footer } from "./footer"
 import { Header } from "./header"
 import { MfaBox } from "./mfa-box"
 import { NewPasswordBox } from "./new-password-box"
+import { QuantityStepper } from "./quantity-stepper"
 
-export { LoginBox, Footer, Header, MfaBox, NewPasswordBox }
+export { LoginBox, Footer, Header, MfaBox, NewPasswordBox, QuantityStepper }
