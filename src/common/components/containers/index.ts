c8d50181f781dfcad5ddd9b8e0efa635d20aee9c
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 20:29:11 +0100

    Update index.ts
    Add Tab to containers

--- src/common/components/containers/index.ts
+++ src/common/components/containers/index.ts
@@ -1,5 +1,5 @@
 import { ChallengeForm } from "./challenge-form"
 import { Hero } from "./hero"
 import { Layout } from "./layout"
-import { TabBox } from "./tab-box"
-export { ChallengeForm, Hero, Layout, TabBox }
+import { TabBox, Tab } from "./tab-box"
+export { ChallengeForm, Hero, Layout, TabBox, Tab }
