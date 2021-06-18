a7a4bb151dba3a4eafdd50add1b171277d0d5f00
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 12:05:51 +0100

    Setup code coverage

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -12,11 +12,9 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
-/**
- * @type {Cypress.PluginConfig}
- */
-// eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+const plugins = (on, config) => {
+  require("@cypress/code-coverage/task")(on, config)
+  return config
 }
+
+export default plugins
--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -13,7 +13,7 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-// Import commands.js using ES2015 syntax:
+import "@cypress/code-coverage/support"
 import "./commands"
 
 // Alternatively you can use CommonJS syntax:
