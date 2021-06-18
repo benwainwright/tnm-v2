823f4aa06dfbbe19b256c4e8b5c40bc3a1ffba3b
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 20:54:01 +0100

    Added margin for safari

--- src/common/components/containers/tab-box/tab-button.tsx
+++ src/common/components/containers/tab-box/tab-button.tsx
@@ -11,6 +11,7 @@ const TabButton: FC<TabButtonProps> = (props) => {
   const StyledButton = styled.button`
     font-family: "Acumin Pro", Arial, sans-serif;
     width: calc(100% / ${props.tabListLength});
+    margin: 0;
     border: 0;
     cursor: pointer;
     padding: 1rem 3rem;
