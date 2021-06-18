5bda2d40c299c7d9e96ea3310acfa1e731afd681
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 19:31:19 +0100

    fix type checks

--- src/common/components/containers/tab-box/tab-button.spec.tsx
+++ src/common/components/containers/tab-box/tab-button.spec.tsx
@@ -5,37 +5,45 @@ import { act } from "react-dom/test-utils"
 
 describe("The <TabButton> component", () => {
   it("renders without error", () => {
-    shallow(<TabButton />)
+    shallow(<TabButton tabListLength={1} />)
   })
 
   it("has a bottom border when inactive", () => {
-    const button = renderer.create(<TabButton />).toJSON()
+    const button = renderer.create(<TabButton tabListLength={1} />).toJSON()
     expect(button).toHaveStyleRule("border-bottom", `1px solid black`)
   })
 
   it("has no border when active", () => {
-    const button = renderer.create(<TabButton active />).toJSON()
+    const button = renderer
+      .create(<TabButton tabListLength={1} active />)
+      .toJSON()
     expect(button).toHaveStyleRule("border-bottom", `0`)
   })
 
   it("has grey background when inactive", () => {
-    const button = renderer.create(<TabButton />).toJSON()
+    const button = renderer.create(<TabButton tabListLength={1} />).toJSON()
     expect(button).toHaveStyleRule("background", `#E3E3E3`)
   })
 
   it("has white background when active", () => {
-    const button = renderer.create(<TabButton active />).toJSON()
+    const button = renderer
+      .create(<TabButton tabListLength={1} active />)
+      .toJSON()
     expect(button).toHaveStyleRule("background", `white`)
   })
 
   it("renders its children", () => {
-    const wrapper = shallow(<TabButton>Some Text</TabButton>)
+    const wrapper = shallow(<TabButton tabListLength={1}>Some Text</TabButton>)
     expect(wrapper.text()).toInclude("Some Text")
   })
 
   it("passes the onClick prop", () => {
     const onClick = jest.fn()
-    const wrapper = shallow(<TabButton onClick={onClick}>Some Text</TabButton>)
+    const wrapper = shallow(
+      <TabButton tabListLength={1} onClick={onClick}>
+        Some Text
+      </TabButton>
+    )
 
     act(() => {
       wrapper.find("button").simulate("click")
