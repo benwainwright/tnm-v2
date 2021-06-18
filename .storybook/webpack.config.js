0d9a81a8e67dcab112b5ae55f974e12c17f8f189
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-18 22:44:59 +0100

    Fixed storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,6 +3,43 @@ const path = require("path")
 module.exports = async ({ config }) => {
   const rules = config.module.rules
 
+  config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
+
+  config.module.rules[0].use[0].options.presets = [
+    require.resolve("@babel/preset-react"),
+    require.resolve("@babel/preset-env"),
+  ]
+  config.module.rules[0].use[0].options.plugins = [
+    // use @babel/plugin-proposal-class-properties for class arrow functions
+    require.resolve("@babel/plugin-proposal-class-properties"),
+    // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+    require.resolve("babel-plugin-remove-graphql-queries"),
+  ]
+  // Prefer Gatsby ES6 entrypoint (module) over commonjs (main) entrypoint
+  config.resolve.mainFields = ["browser", "module", "main"]
+
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve("babel-loader"),
+    options: {
+      presets: [["react-app", { flow: false, typescript: true }]],
+      plugins: [
+        [
+          "module-resolver",
+          {
+            alias: {
+              "@common": "./src/common",
+            },
+          },
+        ],
+        require.resolve("@babel/plugin-proposal-class-properties"),
+        // use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
+        require.resolve("babel-plugin-remove-graphql-queries"),
+      ],
+    },
+  })
+  config.resolve.extensions.push(".ts", ".tsx")
+
   const fileLoaderRule = rules.find((rule) => rule.test.test(".svg"))
   const test = /\.(inline.svg|woff|woff2|ttf|otf)$/
   fileLoaderRule.exclude = test
@@ -10,13 +47,6 @@ module.exports = async ({ config }) => {
   config.module.rules.push({
     test: /\.inline.svg$/,
     use: ["@svgr/webpack"],
-  })
-
-  config.module.rules.push({
-    test: /\.m?js/,
-    resolve: {
-      fullySpecified: false,
-    },
   })
 
   config.module.rules.push({
