bdb565dedc8d8396cd0f62821923685fc0dc47a6
Ben Wainwright <ben.wainwright@bbc.co.uk>
2021-06-20 19:49:07 +0100

    pnp stuff

---
+++ cypress/plugins/index.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+module.exports = (on, config) => {
+  const pnp = require("../../.pnp.js")
+  pnp.setup()
+
+  const targetModule = "."
+  require(pnp.resolveToUnqualified(
+    "@cypress/code-coverage/task",
+    targetModule
+  ))(on, config)
+
+  const webpackPreprocessor = require(pnp.resolveToUnqualified(
+    "@cypress/webpack-preprocessor",
+    targetModule
+  ))
+
+  const PnpWebpackPlugin = require(pnp.resolveToUnqualified(
+    "pnp-webpack-plugin",
+    targetModule
+  ))
+
+  const options = {
+    webpackOptions: {
+      resolve: {
+        plugins: [PnpWebpackPlugin],
+      },
+      resolveLoader: {
+        plugins: [PnpWebpackPlugin.moduleLoader(module)],
+      },
+    },
+    watchOptions: {},
+  }
+
+  on("file:preprocessor", webpackPreprocessor(options))
+  return config
+}
