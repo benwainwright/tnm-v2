40b95111169e27df4203f9ea0bbab502b622801a
Ben Wainwright <ben.wainwright@cinch.co.uk>
2021-07-25 02:58:32 +0100

    Fixed build

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,7 +44,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/common/assets/images`,
+        path: `${__dirname}/src/app/assets/images`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -58,7 +58,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/common/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/app/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
